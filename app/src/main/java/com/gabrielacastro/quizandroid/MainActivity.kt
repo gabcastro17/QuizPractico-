609037d60ec7194015187890665eb8c7b0a9ec18
Gabriela Castro <gcastro218@unab.edu.co>
2026-02-27 17:37:38 -0500

    UI implementada - Pantalla 2

--- app/src/main/java/com/gabrielacastro/quizandroid/MainActivity.kt
+++ app/src/main/java/com/gabrielacastro/quizandroid/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,14 +12,21 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.gabrielacastro.quizandroid.ui.theme.QuizAndroidGabrielaCastroTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +34,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            pantalla1()
+            pantalla2()
         }
     }
 }
@@ -55,5 +63,29 @@ fun pantalla1(){
 
         }
     }
+}
+@Composable
+fun pantalla2(){
+    Column(modifier = Modifier.padding(16.dp).fillMaxWidth()) {
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly)
+        {
+            val painter = painterResource(R.drawable.ic_launcher_background)
+            Image(
+                painter = painter,
+                contentDescription = null,
+                modifier = Modifier.clip(CircleShape)
+            )
+        }
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly)
+        {
+            Text(text = "Juan Pérez", fontWeight = FontWeight.Bold, fontSize = 20.sp, modifier = Modifier.padding(16.dp))
+        }
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly)
+        {
+            Text(text = "Desarrollador Android apasionado por la tecnologia y el diseño", fontSize = 20.sp, modifier = Modifier.padding(16.dp))
+        }
 
+
+
+    }
 }
