8ba5575aab154fd6911924ec0b768db97a71a912
Gabriela Castro <gcastro218@unab.edu.co>
2026-02-27 17:15:46 -0500

    UI implementada - Pantalla 1

--- app/src/main/java/com/gabrielacastro/quizandroid/MainActivity.kt
+++ app/src/main/java/com/gabrielacastro/quizandroid/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gabrielacastro.quizandroid.ui.theme.QuizAndroidGabrielaCastroTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +26,34 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            QuizAndroidGabrielaCastroTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            pantalla1()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    QuizAndroidGabrielaCastroTheme {
-        Greeting("Android")
+fun pantalla1(){
+    Column(modifier = Modifier.padding(16.dp).fillMaxSize(), verticalArrangement = Arrangement.SpaceBetween)
+    {
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center)
+        {
+            Text(text = "Encabezado", modifier = Modifier.background(Color.Yellow).padding(8.dp))
+
+        }
+
+        Row(modifier = Modifier.padding(16.dp).fillMaxWidth().background(Color.Green), horizontalArrangement = Arrangement.SpaceEvenly)
+        {
+            Text(text = "Item 1", modifier = Modifier.padding(16.dp).background(Color.Yellow))
+            Text(text = "Item 2", modifier = Modifier.padding(16.dp).background(Color.Blue))
+            Text(text = "Item 3",  modifier = Modifier.padding(16.dp).background(Color.Red))
+        }
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center)
+        {
+            Text(text = "Pie de pagina", modifier = Modifier.padding(16.dp).background(Color.LightGray))
+
+        }
     }
+
 }
